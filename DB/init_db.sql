3776faf4ea2f9f88ed8ed5a44cc663d800d72780
Petri Airio <petri.j.airio@gmail.com>
2014-11-03 13:17:55 +0200

    Add also % user

--- DB/init_db.sql
+++ DB/init_db.sql
@@ -1,6 +1,7 @@
 DROP DATABASE  IF EXISTS hokan_ng_dev;
 
 CREATE DATABASE `hokan_ng_dev` CHARACTER SET utf8 COLLATE utf8_general_ci;
-GRANT ALL ON `hokan_ng_dev`.* TO `hokan_ng`@localhost IDENTIFIED BY 'hokan_ng';
+GRANT ALL ON `hokan_ng_dev`.* TO `hokan_ng`@`localhost` IDENTIFIED BY 'hokan_ng';
+GRANT ALL ON `hokan_ng_dev`.* TO `hokan_ng`@`%` IDENTIFIED BY 'hokan_ng';
 
 FLUSH PRIVILEGES;
